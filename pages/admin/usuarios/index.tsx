eeb26ddd26e2a80fad6237f1578c9e68ff02a9df
laura <posadalaura57@gmail.com>
2021-11-03 12:37:33 -0500

    Clase 1

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
-
 const index = () => {
-    return (
-      <div>
-          Esta es la página de usuarios
-      </div>
-    )
-  }
+  return (
+    <div>
+      <div>Pagina de admin de usuarios</div>
+      <Link href='/'>
+        <a>Ir al Home</a>
+      </Link>
+    </div>
+  );
+};
 
 export default index
